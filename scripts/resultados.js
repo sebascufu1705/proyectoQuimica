d9c53e536752051d211ea437f56ae1277797de5a
sebastian velasquez <sebasm4a1@hotmail.com>
2020-12-05 22:07:41 -0500

    finalizado el memoria

--- scripts/resultados.js
+++ scripts/resultados.js
@@ -1,11 +1,16 @@
-var tiempo = localStorage.getItem('tiempo');
-var progreso = localStorage.getItem('progreso');
-var puntuacion = localStorage.getItem('puntuacion');
+
+var tiempo = 0;
+var progreso = 0;
+var puntuacion = 0;
+
+ tiempo = localStorage.getItem('tiempo');
+ progreso = localStorage.getItem('progreso');
+ puntuacion = localStorage.getItem('puntuacion');
 function showResults(){
     id=document.getElementById("result");
-    id.innerHTML= ` <h1>Tiempo: ${tiempo}</h1>
-    <div class="puntuacion"> <h1>Puntuacion: X${progreso}</h1><img src="./img/estrella.png" alt="estrella"></div>
-    <h1>Progreso: ${puntuacion}/13</h1>`
+    id.innerHTML= ` <h1>Tiempo: ${tiempo}s restantes</h1>
+    <div class="puntuacion"> <h1>Puntuacion: X${puntuacion}</h1><img src="./img/estrella.png" alt="estrella"></div>
+    <h1>Progreso: ${progreso}/13</h1>`
 }
 showResults();
 console.log(tiempo);
